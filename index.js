08660bc6d3c40d89a6cc5c91bbbc48161862995b
Staicy Stadtler <staicy@hotmail.de>
2023-08-30 11:42:43 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const person = {
+  name: "Ralf",
+  job: "Coach",
+};
+let isRalfCoach;
+if (person.job === "Coach") {
+  isRalfCoach = true;
+} else {
+  isRalfCoach = false;
+}
+console.log(isRalfCoach);
