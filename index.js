e8bfb487151059a6436e090498eadc4439d2fa58
Staicy Stadtler <staicy@hotmail.de>
2023-08-30 11:56:08 +0200

    solution for the exercises

--- index.js
+++ index.js
@@ -8,6 +8,25 @@ if (person.job === "Dancer") {
 } else {
   isRalfCoach = false;
 }
-console.log(isRalfCoach); // This console.log results in yellow
+//console.log(isRalfCoach); // This console.log results in yellow
+//console.log(isRalfCoach ? "true" : "false"); // this is white
 
-console.log(isRalfCoach ? "true" : "false"); // this is white
+const age = 19;
+let stageOfLife;
+if (age <= 14) {
+  stageOfLife = "child";
+} else if (age <= 18) {
+  stageOfLife = "teenager";
+} else {
+  stageOfLife = "adult";
+}
+console.log(stageOfLife);
+
+console.log(
+  (stageOfLife = age <= 14 ? "child" : age <= 18 ? "teenager" : "adult")
+);
+// First part: is stageOfLife <= 14? If yes, print "child"
+// Second part is executed when the first expression is false
+// age <= 18 is asked, when first part false.
+// Second part evaluates to true, then teenager is printed
+// If age >18, adult is printed
